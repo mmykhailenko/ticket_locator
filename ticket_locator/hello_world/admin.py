9d2807c59d3b88585ad07410a131311da66005f2
Vitalii K_M <cj_vizer@ukr.net>
2021-04-27 22:22:35 +0300

    Created base models

--- ticket_locator/hello_world/admin.py
+++ ticket_locator/hello_world/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, SearchHistory
 
-# Register your models here.
+from django import forms
+from django.forms import ModelForm, PasswordInput
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'active')
+
+
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'departure_date', 'arrival_date')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(SearchHistory, SearchHistoryAdmin)
+
+
--- ticket_locator/hello_world/models.py
+++ ticket_locator/hello_world/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django import forms
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField('E-Mail')
+    password = models.CharField(max_length=32)
+    # role = models.ForeignKey('role') ?
+    active = models.BooleanField('Activity', default=False)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
+
+    def save(self, **kwargs):
+        some_salt = 'some_salt'
+        self.password = make_password(self.password, some_salt)
+        super().save(**kwargs)
+
+
+class SearchHistory(models.Model):
+    departure_city = models.CharField('Departure city', max_length=128)
+    arrival_city = models.CharField('Arrival city', max_length=128)
+    departure_date = models.DateTimeField('Departure date')
+    arrival_date = models.DateTimeField('Arrival date')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Search history'
+        verbose_name_plural = 'Search history'
+
+    def __str__(self):
+        return f"{self.departure_city} -> {self.arrival_city}"
+
+
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
