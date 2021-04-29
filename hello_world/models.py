eace668c4381e0e51a6626b9d9c8ef1806c0104d
Oleksiy Yurchenko <oleksiy.yurchenko@gmail.com>
2021-04-29 10:06:57 +0300

    Created models and migrations

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class SearchHistoryPost(models.Model):
+
+    departure_city = models.CharField(max_length=40)
+    arrival_city = models.CharField(max_length=40)
+    departure_date = models.DateField()
+    arrival_date = models.DateField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
