4d103c25df1c241b51bfd5f5f15a2060b9dc2822
Nina Prus <nina.prus95@gmail.com>
2021-05-16 09:17:34 +0300

    Added .env file

--- ticket_locator/settings.py
+++ ticket_locator/settings.py
@@ -18,8 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+import environ
+# Initialise environment variables
+
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*@!vb-#l9g%zryddqqo^&yij6a-4vf3*@bi$sf)=*wq_sdr@l'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
