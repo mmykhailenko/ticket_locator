8d3630103b7bba864fa4da1d8d537fdec83f4a75
Cj_Vizer <cjvizer@gmail.com>
2021-05-31 16:39:27 +0300

    Implemented authentication, login, registration. Created telegram bot. Changed Jinja templates

--- hello_world/forms.py
+++ hello_world/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django import forms
 
 from .models import User
@@ -20,17 +20,52 @@ class SearchAirRouteForm(forms.Form):
     ##################### Widgets ############################
     departure_airport_widget = forms.TextInput(
         attrs={
-            "placeholder": "IATA format",
+            "placeholder": "Откуда:",
             "required": True,
-            "class": "form-control",
+            "class": "form-control mt-4",
             "type": "text",
+            "pattern": "[A-Z]{3}",
+            "title": 'IATA код аэропорта. Пример: "BCN", "AMS" ...'
+        }
+    )
+    arrival_airport_widget = forms.DateInput(
+        attrs={
+            "placeholder": "Куда:",
+            "required": True,
+            "class": "form-control mt-4",
+            "type": "text",
+            "pattern": "[A-Z]{3}",
+            "title": 'IATA код аэропорта. Пример: "BCN", "AMS" ...'
         }
     )
     departure_date_widget = forms.DateInput(
-        attrs={"required": True, "class": "form-control", "type": "date"}
+        attrs={
+            "required": True,
+            "class": "form-control mt-4",
+            "type": "date"
+        }
     )
     ######################### Form ###############################
     departure_airport = forms.CharField(max_length=3, widget=departure_airport_widget)
-    arrival_airport = forms.CharField(max_length=3, widget=departure_airport_widget)
+    arrival_airport = forms.CharField(max_length=3, widget=arrival_airport_widget)
     departure_date = forms.DateField(widget=departure_date_widget)
     direct_flight = forms.BooleanField(required=False)
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.CharField(widget=forms.TextInput())
+    password1 = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2', )
+
+
+class LoginForm(AuthenticationForm):
+    email = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
---
+++ hello_world/management/commands/bot.py
@@ -0,0 +1,81 @@
+import re
+from django.core.management.base import BaseCommand
+from ticket_locator import settings
+from telegram import Bot
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater
+from telegram.utils.request import Request
+
+from hello_world.views import FlightSearchView
+from . import constants
+
+
+def start(update: Update, _: CallbackContext) -> None:
+    user_name = update.message.chat.first_name
+    reply_text = f'\U00002708 Привет {user_name}!\n' \
+                 f'\U00002708 Рады видеть тебя:)\U0000270A\n\git statusn' \
+                 f'\U00002708 Нужна помощь? Введите /help'
+    update.message.reply_text(text=reply_text)
+
+
+def search(update: Update, _: CallbackContext) -> None:
+    text = update.message.text.split(' ')
+    if len(text) > 2:
+        if re.search('[A-Za-z]{3}', text[0]) and re.search('[A-Za-z]{3}', text[1]) and re.search('[0-9]{8}', text[2]):
+            request_data = {
+                'departure_airport': text[0].upper(),
+                'arrival_airport': text[1].upper(),
+                'departure_date': text[2].upper(),
+                'direct_flight': text[3] if len(text) > 3 and text[3].upper() == 'DIRECT' else None
+            }
+            response = FlightSearchView().get_air_info(request_data)
+            if response:
+                for flights in response:
+                    reply_text = '--------------------\n'
+                    reply_text += 'С пересадкой\n' if len(flights) > 1 else 'Прямой\n'
+                    reply_text += '--------------------\n\n'
+                    for flight_item in flights:
+                        reply_text += f'Из: {flight_item["DepartureAirport"]} В: {flight_item["ArrivalAirport"]}\n' \
+                                      f'Рейс: {flight_item["Airline"]}{flight_item["FlightNumber"]}\n' \
+                                      f'Время вылета: {" ".join(flight_item["DepartureTime"].split("T"))}\n' \
+                                      f'Время прибытия: {" ".join(flight_item["ArrivalTime"].split("T"))}\n\n'
+                    update.message.reply_text(text=reply_text)
+            else:
+                update.message.reply_text(text=constants.BOT_NO_RESULT_TEXT)
+        else:
+            update.message.reply_text(text=constants.BOT_ERROR_TEXT)
+    else:
+        update.message.reply_text(text=constants.BOT_ERROR_TEXT)
+
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(text=constants.BOT_HELP_TEXT)
+
+
+class Command(BaseCommand):
+    help = 'Телеграм-бот'
+
+    def handle(self, *args, **options):
+        request = Request(
+            connect_timeout=1.0,
+            read_timeout=2.0,
+        )
+        bot = Bot(
+            request=request,
+            token=settings.TELEGRAM_BOT_TOKEN,
+            base_url=settings.TELEGRAM_BOT_URL,
+        )
+
+        updater = Updater(
+            bot=bot,
+            use_context=True,
+        )
+
+        updater.dispatcher.add_handler(CommandHandler('start', start))
+        updater.dispatcher.add_handler(CommandHandler('help', help_command))
+        updater.dispatcher.add_handler(CommandHandler('search', search))
+        updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, search))
+
+        updater.start_polling()
+        updater.idle()
